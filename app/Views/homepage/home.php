a5b347ec492d3c502c6a2404e68372e4684e8341
nurulauiakh31 <nurulauliakhoerunnisa@gmail.com>
2022-09-01 09:29:00 +0700

    mitra & regulasi

--- app/Views/homepage/home.php
+++ app/Views/homepage/home.php
@@ -7,6 +7,7 @@
     <title>Home | Inkindo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://code.iconify.design/iconify-icon/1.0.0-beta.3/iconify-icon.min.js"></script>
 </head>
 <style>
     .navbar-background {
@@ -15,6 +16,30 @@
 
     .caption-background {
         background: rgba(255, 255, 255, 0.7);
+    }
+
+    .center {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .a {
+        text-decoration: none;
+    }
+    .kategori {
+        font-size: 18px; 
+        margin: 4.5px; 
+        text-decoration:none;
+        padding-top: 4px;
+        color: black;
+    }
+    .kategori:hover {
+        background-color: black;
+        color: white;
+
     }
 </style>
 
@@ -53,41 +78,130 @@
             </div>
         </div>
     </div>
-
-    <div class="container d-flex flex-column align-items-center py-4 py-xl-5">
-        <div class="row mb-5">
-            <div class="col-md-8 col-xl-6 text-center mx-auto">
-                <h2>Profil</h2>
-                <p class="w-lg-50">Curae hendrerit donec commodo hendrerit egestas tempus, turpis facilisis nostra nunc. Vestibulum dui eget ultrices.</p>
-            </div>
-        </div>
-        <div class="row gy-4 w-100" style="max-width: 800px;">
-            <div class="col-12">
-                <div class="card"><img class="card-img w-100 d-block shadow" src="Office 1.jpg" style="opacity: 1;" />
-                    <div class="card-img-overlay text-center d-flex flex-column justify-content-center align-items-center p-4">
-                        <h4 class="bg-white shadow">Spot Gallery</h4>
-                        <p class="bg-white shadow">Volutpat habitasse risus posuere, commodo fusce donec. Turpis donec tristique.</p>
+    <br>
+    <div class="container">
+        <h2 class="text-center">Regulasi</h2>
+        <br>
+        <div class="center">
+            <div class="col">
+                <div class="row">
+                    <div class="col-8">
+                        <div class="row">
+                            <h5>Billing Rate INKINDO Tahun 2022</h5>
+                            <h6>Billing Rate INKINDO Tahun 2022</h6>
+                            <div class="col">
+                                <iconify-icon icon="akar-icons:download"></iconify-icon>
+                                <a href="https://www.inkindo-dki.org/download-regulasi/billing-rate-inkindo-tahun-2021" style="text-decoration:none">Download</a>
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="row">
+                            <h5>Ketetapan Dewan Pengurus Nasional Konsultan Indonesia Ikatan Nasional Konsultan Indonesia Nomor : 1 ITAP.DPN/ll/2019</h5>
+                            <h6>Ketetapan Dewan Pengurus Nasional Konsultan Indonesia Ikatan Nasional Konsultan Indonesia Nomor : 1 ITAP.DPN/ll/2019 Tentang Peraturan Organisasi</h6>
+                            <div class="col">
+                                <iconify-icon icon="akar-icons:download"></iconify-icon>
+                                <a href="https://www.inkindo-dki.org/download-regulasi/ketetapan-dewan-pengurus-nasional-konsultan-indonesia-ikatan-nasional-konsultan-indonesia-nomor-1-itapdpnll2019" style="text-decoration:none">Download</a>
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="row">
+                            <h5>Roadmap Menuju INKINDO Emas 2030</h5>
+                            <h6>Roadmap Menuju Inkindo Emas 2030 "INKINDO Mandiri Demi Keunggulan Negeri"</h6>
+                            <div class="col">
+                                <iconify-icon icon="akar-icons:download"></iconify-icon>
+                                <a href="https://www.inkindo-dki.org/download-regulasi/roadmap-menuju-inkindo-emas-2030" style="text-decoration:none">Download</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6">
-                <div class="card"><img class="card-img w-100 d-block" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png" />
-                    <div class="card-img-overlay text-center d-flex flex-column justify-content-center align-items-center p-4">
-                        <h4>Sub gallery</h4>
-                        <p>Volutpat habitasse risus posuere, commodo fusce donec. Turpis donec tristique.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6">
-                <div class="card"><img class="card-img w-100 d-block" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png" />
-                    <div class="card-img-overlay text-center d-flex flex-column justify-content-center align-items-center p-4">
-                        <h4>Sub gallery</h4>
-                        <p>Volutpat habitasse risus posuere, commodo fusce donec. Turpis donec tristique.</p>
+                    <div class="col-2">
+                        <div class="row">
+                            <h5 style="margin:3px;">Kategori</h5>
+                        </div>
+                        <div class="row" style="width: 350px; height: 48px; margin: 3px;">
+                            <a href="#" class="kategori border border-gray" type="button">Regulasi Inkindo</a>
+                        </div>
+                        <div class="row" style="width: 350px; height: 48px; margin:3px;">
+                            <a href="#" class="kategori border border-gray" type="button">Regulasi Jasa Konsultasi Konstruksi</a>
+                        </div>
+                        <div class="row" style="width: 350px; height: 48px; margin:3px;">
+                            <a href="#" class="kategori border border-gray" type="button">Regulasi Jasa Konsultasi Non-Konstruksi</a>
+                        </div>
+                        <div class="row" style="width: 350px; height: 48px; margin:3px;">
+                            <a href="#" class="kategori border border-gray" type="button">Regulasi Jasa Konsultasi Umum</a>
+                        </div>
+                        <div class="row" style="width: 350px; height: 48px; margin:3px;">
+                            <a href="#" class="kategori border border-gray" type="button">Regulasi Terkait</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <br>
+    <div class="container">
+        <h2 class="text-center">Mitra</h2>
+        <div class="center">
+            <div class="container">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="row">
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/dekson.jpg'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/seven.jpg'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/jiyu.jpg'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/Daikin.jpg'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/LG.png'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/CEC.jpg'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row">
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/Majalah-Konstruksi.png'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/Bromindo.png'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/HITACHI.png'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/granito.jpg'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/LG.png'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="col-lg-2">
+                                    <img src="<?= base_url('../assets/img/CEC.jpg'); ?>" class="d-block w-100" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 
